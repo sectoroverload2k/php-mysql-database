8518f698e970b60cf5b75251abfbad091acab1f5
Anthony Linsday <anthony@linsday.net>
2024-12-17 10:33:20 -0500

    1.0.12

--- src/MySQL.php
+++ src/MySQL.php
@@ -13,13 +13,58 @@ class MySQL
         $this->db_handle = $db_handle;
     }
     // Run a query
-    public function query($statement)
+    public function query($statement, $params = [])
     {
-        $this->result = mysqli_query($this->db_handle, $statement);
-        $this->error = mysqli_error($this->db_handle);
-        $this->errno = mysqli_errno($this->db_handle);
+		// Prepare the statement
+		$this->stmt = mysqli_prepare($this->db_handle, $statement);
+		if(!$this->stmt) {
+			$this->error = mysqli_error($this->db_handle);
+			$this->errno = mysqli_errno($this->db_handle);
+			return false;
+		}
+
+		// Bind the parameters if provided
+		if (!empty($params)){
+			$types = $this->getParamTypes($params);
+			mysqli_stmt_bind_param($this->stmt, $types, ...$params);
+		}
+
+		// execute the query
+		if(!mysqli_stmt_execute($this->stmt)){
+			$this->error = mysqli_stmt_error($this->stmt);
+			$this->errno = mysqli_stmt_errno($this->stmt);
+			return false;
+		}
+
+        $this->result = mysqli_stmt_get_result($this->stmt);
+
         return $this->result;
     }
+
+	// Helper method to determine parameter types
+	private function getParamTypes($params)
+	{
+		$types = '';
+		foreach($params as $param){
+			if(is_int($param)){
+				$types .= 'i';
+			} elseif(is_double($param)){
+				$types .= 'd';
+			} elseif($is_string($param)){
+				$types .= 's';
+			} else {
+				$types .= 'b'; // blob
+			}
+		}
+		return $types;
+	}
+
+	// Fetch a single row // backwards compat
+	public function fetch()
+	{
+		return $this->fetchAssoc();
+	}
+
     // Fetch num rows
     public function numRows()
     {
@@ -43,7 +88,7 @@ class MySQL
     public function fetchAll()
     {
         $return = [];
-        while($row = $this->fetchAssoc()) {
+        while($row = $this->fetch()) {
             $return[] = $row;
         }
         return $return;
