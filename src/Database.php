5621f32f4d5fca026c7459470b996c265712a6e7
Anthony Linsday <anthony@linsday.net>
2025-07-20 01:33:55 +0000

    added prepare/exec for statements

--- src/Database.php
+++ src/Database.php
@@ -83,6 +83,68 @@ class Database
             return $mysql;
         }
     }
+    
+    /**
+     * Prepare a SQL statement for execution
+     * 
+     * @param string $statement The SQL query to prepare
+     * @return MySQL|null Returns MySQL object on success or null on failure
+     */
+    public function prepare($statement)
+    {
+        $mysql = new MySQL($this->db_handle);
+        $mysql->prepare($statement);
+        $this->set_error($mysql->error());
+        if($mysql->error()) {
+            throw new DBErrorException($mysql->error());
+            return null;
+        } else {
+            return $mysql;
+        }
+    }
+    
+    /**
+     * Bind parameters to a prepared statement
+     * 
+     * @param MySQL $stmt The prepared statement object
+     * @param mixed $params Parameter or array of parameters to bind
+     * @param string $types Optional string of types for the parameters
+     * @return MySQL The prepared statement object
+     */
+    public function bind_param($stmt, $params, $types = null)
+    {
+        $stmt->bind_param($params, $types);
+        $this->set_error($stmt->error());
+        if($stmt->error()) {
+            throw new DBErrorException($stmt->error());
+        }
+        return $stmt;
+    }
+    
+    /**
+     * Execute a prepared statement
+     * 
+     * @param MySQL $stmt The prepared statement object
+     * @return MySQL The prepared statement object with results
+     */
+    public function execute($stmt)
+    {
+        $stmt->execute();
+        $this->set_error($stmt->error());
+        if($stmt->error()) {
+            switch($stmt->errno()) {
+                // delete fk constraint
+                case 1451: // fk delete constraint
+                    throw new ForeignKeyException('Cannot delete or update this record. Other data depends on this record.');
+                case 1452: // fk add/update constraint
+                    throw new ForeignKeyException('Cannot add or update this record. Other information is required.');
+                    break;
+                default:
+                    throw new DBErrorException($stmt->error());
+            }
+        }
+        return $stmt;
+    }
     // Gets the first column of the first row
     public function get_one($statement)
     {
--- src/MySQL.php
+++ src/MySQL.php
@@ -8,6 +8,7 @@ class MySQL
     private $result;
     private $error;
     private $errno;
+    private $stmt;
     public function __construct($db_handle)
     {
         $this->db_handle = $db_handle;
@@ -53,9 +54,11 @@ class MySQL
 	{
 		$types = '';
 		foreach($params as $param){
-			if(is_int($param)){
+			if(is_null($param)){
+				$types .= 's'; // NULL values as strings
+			} elseif(is_int($param)){
 				$types .= 'i';
-			} elseif(is_double($param)){
+			} elseif(is_double($param) || is_float($param)){
 				$types .= 'd';
 			} elseif(is_string($param)){
 				$types .= 's';
@@ -115,5 +118,98 @@ class MySQL
             return $this->errno;
         }
         return null;
+    }
+    
+    /**
+     * Prepare a statement for execution
+     * 
+     * @param string $query The SQL query to prepare
+     * @return bool|mysqli_stmt Returns statement object on success or false on failure
+     */
+    public function prepare($query)
+    {
+        $this->stmt = mysqli_prepare($this->db_handle, $query);
+        if(!$this->stmt) {
+            $this->error = mysqli_error($this->db_handle);
+            $this->errno = mysqli_errno($this->db_handle);
+            return false;
+        }
+        
+        return $this->stmt;
+    }
+    
+    /**
+     * Bind parameters to the prepared statement
+     * 
+     * @param mixed $params Parameter or array of parameters to bind
+     * @param string $types Optional string of types for the parameters
+     * @return bool True on success or false on failure
+     */
+    public function bind_param($params, $types = null)
+    {
+        if(!$this->stmt) {
+            $this->error = 'No prepared statement exists';
+            return false;
+        }
+        
+        // Convert to array if single parameter
+        if(!is_array($params)) {
+            $params = [$params];
+        }
+        
+        // Auto-detect types if not provided
+        if($types === null) {
+            $types = $this->getParamTypes($params);
+        }
+        
+        // Bind parameters
+        if(!mysqli_stmt_bind_param($this->stmt, $types, ...$params)) {
+            $this->error = mysqli_stmt_error($this->stmt);
+            $this->errno = mysqli_stmt_errno($this->stmt);
+            return false;
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Execute a prepared statement
+     * 
+     * @return bool|mysqli_result Returns result object on success or false on failure
+     */
+    public function execute()
+    {
+        if(!$this->stmt) {
+            $this->error = 'No prepared statement exists';
+            return false;
+        }
+        
+        // Execute the statement
+        if(!mysqli_stmt_execute($this->stmt)) {
+            $this->error = mysqli_stmt_error($this->stmt);
+            $this->errno = mysqli_stmt_errno($this->stmt);
+            return false;
+        }
+        
+        // Get the result
+        $this->result = mysqli_stmt_get_result($this->stmt);
+        
+        return $this->result;
+    }
+    
+    /**
+     * Close the prepared statement
+     * 
+     * @return bool True on success or false on failure
+     */
+    public function close_stmt()
+    {
+        if(!$this->stmt) {
+            return true; // No statement to close
+        }
+        
+        $result = mysqli_stmt_close($this->stmt);
+        $this->stmt = null;
+        return $result;
     }
 }
